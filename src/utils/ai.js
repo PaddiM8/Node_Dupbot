7d9fc7ae5d75f40aa72d1ee4e02d332340abf69c
PaddiM8 <paddim8@pm.me>
2018-06-21 01:38:22 +0200

    Update ai.js

--- src/utils/ai.js
+++ src/utils/ai.js
@@ -8,7 +8,7 @@ function get(Client, msg) {
     if (!msg.input_ai) return;
     let request = app.textRequest(convert(msg.input_ai), {
         sessionId: msg.author.id,
-        timezone: "Europa/Madrid"
+        timezone: "Europa/London"
     });
 
     request.on("response", function(response) {
